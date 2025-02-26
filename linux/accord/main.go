7c43ce918a245cae08eca24e44e8161e17a4523f
Mark Tsizis <tsizism@yahoo.ca>
2025-02-25 21:03:46 -0500

    Encapsulating goroutins pattern

--- linux/accord/main.go
+++ linux/accord/main.go
@@ -9,10 +9,10 @@ import (
 )
 
 var rawOrders = []string{
-	`{"productCode":1111, "quantity": 5, "status": 0}`,
+	`{"productCode":1111, "quantity": 5, 	"status": 0}`,
 	`{"productCode":2222, "quantity": 42.3, "status": 1}`,
-	`{"productCode":3333, "quantity": 19, "status": 2}`,
-	`{"productCode":4444, "quantity": 8, "status": 3}`,
+	`{"productCode":3333, "quantity": 19, 	"status": 2}`,
+	`{"productCode":4444, "quantity": 8, 	"status": 3}`,
 }
 
 // var orders = []order{}
@@ -39,13 +39,16 @@ func bufChannel() {
 
 func orderMgmt() {
 	var wg sync.WaitGroup
-	var receivedOrdercCh = make(chan order)
-	var validateOrdercCh = make(chan order)
-	var invalidOrdercCh = make(chan invalidOrder)
+	// var receivedOrdercCh = make(chan order)
+	// var validateOrdercCh = make(chan order)
+	// var invalidOrdercCh = make(chan invalidOrder)
 
 	wg.Add(1)
-	go receiveOrders(receivedOrdercCh)
-	go validateOrders(receivedOrdercCh, validateOrdercCh, invalidOrdercCh)
+	// go receiveOrders(receivedOrdercCh)
+	receivedOrdercCh := receiveOrders() // ro <-chan order
+	validateOrdercCh, invalidOrdercCh := validateOrders(receivedOrdercCh) // ro chan oreder, ro chan invalidOrder
+
+	// go validateOrders(receivedOrdercCh, validateOrdercCh, invalidOrdercCh)
 
 	go func(validOrderCh <-chan order, invalidOrderCh <-chan invalidOrder) { // inCh rcv from channel
 		loop : for{
@@ -91,35 +94,88 @@ func orderMgmt() {
 
 }
 
-func validateOrders(inCh <-chan order, outCh chan<- order, errCh chan<- invalidOrder) {
+
+func validateOrders(inCh <-chan order)  (<-chan order, <-chan invalidOrder) {
 	// order := <- inCh // inCh rcv from ch
 
-	for order := range inCh {
-		if order.Quantity <= 0 {
-			errCh <- invalidOrder{Order: order, Err: errors.New("invalid negavtive order quantity")}
-			// errCh <- invalidOrder { Order: order, Err: fmt.Errorf("invalid order quantity %f", order.Quantity) }
-		} else {
-			// fmt.Printf("Valid order  %v\n", order)
-			outCh <- order // outCh snd to ch
+	outCh := make(chan order)
+	errCh := make(chan invalidOrder, 1)
+
+	go func () {
+		for order := range inCh {
+			if order.Quantity <= 0 {
+				errCh <- invalidOrder{Order: order, Err: errors.New("invalid negavtive order quantity")}
+				// errCh <- invalidOrder { Order: order, Err: fmt.Errorf("invalid order quantity %f", order.Quantity) }
+			} else {
+				// fmt.Printf("Valid order  %v\n", order)
+				outCh <- order // outCh snd to ch
+			}
 		}
-	}
-	close(outCh)
-	close(errCh)
+		close(outCh)
+		close(errCh)
+	}()
+
+	return outCh, errCh // as ro channel
+
 }
 
-func receiveOrders(outCh chan<- order) {
-	for _, rawOrder := range rawOrders {
-		var newOrder order
 
-		err := json.Unmarshal([]byte(rawOrder), &newOrder)
 
-		if err != nil {
-			log.Println(err)
-			continue
+// // <-chan order ro
+// func validateOrders(inCh <-chan order, outCh chan<- order, errCh chan<- invalidOrder) {
+// 	// order := <- inCh // inCh rcv from ch
+
+// 	for order := range inCh {
+// 		if order.Quantity <= 0 {
+// 			errCh <- invalidOrder{Order: order, Err: errors.New("invalid negavtive order quantity")}
+// 			// errCh <- invalidOrder { Order: order, Err: fmt.Errorf("invalid order quantity %f", order.Quantity) }
+// 		} else {
+// 			// fmt.Printf("Valid order  %v\n", order)
+// 			outCh <- order // outCh snd to ch
+// 		}
+// 	}
+// 	close(outCh)
+// 	close(errCh)
+// }
+
+
+func receiveOrders() <-chan order { 
+	outCh := make(chan order) // rw chan
+
+	go func() {
+		for _, rawOrder := range rawOrders {
+			var newOrder order
+
+			err := json.Unmarshal([]byte(rawOrder), &newOrder)
+
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			// fmt.Printf("Receiving new order %v\n", newOrder)
+			//orders = append(orders, newOrder)
+			outCh <- newOrder
 		}
-		// fmt.Printf("Receiving new order %v\n", newOrder)
-		//orders = append(orders, newOrder)
-		outCh <- newOrder
-	}
-	close(outCh)
+		close(outCh)
+	}()
+
+	return outCh // returm as ro
 }
+
+
+// func receiveOrders(outCh chan<- order) {
+// 	for _, rawOrder := range rawOrders {
+// 		var newOrder order
+
+// 		err := json.Unmarshal([]byte(rawOrder), &newOrder)
+
+// 		if err != nil {
+// 			log.Println(err)
+// 			continue
+// 		}
+// 		// fmt.Printf("Receiving new order %v\n", newOrder)
+// 		//orders = append(orders, newOrder)
+// 		outCh <- newOrder
+// 	}
+// 	close(outCh)
+// }
